3029ec2c9a2d92bfb6c4981180981ef66018ea00
Sehiko <kitinet2005@gmail.com>
2025-11-07 02:30:52 +0200

    PR_6-7

---
+++ src/index.mjs
@@ -0,0 +1,39 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const mockUsers = [
+  { id: 1, username: "anson", displayName: "Anson" },
+  { id: 2, username: "jack", displayName: "Jack" },
+  { id: 3, username: "adam", displayName: "Adam" },
+];
+
+app.get("/", (request, response) => {
+  response.status(201).send({ msg:"Hello!" });
+});
+
+app.get("/api/users", (request, response) => { 
+  response.send(mockUsers);
+}); 
+
+app.get("/api/users/:id", (request, response) => {
+  console.log(request.params);
+  const parsedId = parseInt(request.params.id);
+  if (isNaN(parsedId))
+    return response.status(400).send({ msg: "Bad Request. Invalid ID."
+  });
+  const findUser = mockUsers.find((user) => user.id === parsedId);
+  if (!findUser) return response.sendStatus(404);
+  return response.send(findUser);
+}); 
+
+app.get("/api/products", (request, response) => { 
+  response.send([
+    { id: 123, name: "chicken breast", price: 12.99 }, 
+  ]);
+});
+
+app.listen(PORT, () => {
+  console.log(`Running on Potr ${PORT}`);
+});
